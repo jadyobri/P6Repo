fda03e8d185805935be13cd74704e22d51b9e7d7
Keegan Vogan <kvogan@ucsc.edu>
2025-02-19 15:15:16 -0800

    updated README, added basic_model.py config, changed train.py to 5 epochs

--- P6/src/models/basic_model.py
+++ P6/src/models/basic_model.py
@@ -1,15 +1,43 @@
 from models.model import Model
 from tensorflow.keras import Sequential, layers
 from tensorflow.keras.layers.experimental.preprocessing import Rescaling
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
 from tensorflow.keras.optimizers import RMSprop, Adam
 
 class BasicModel(Model):
     def _define_model(self, input_shape, categories_count):
         # Your code goes here
         # you have to initialize self.model to a keras model
-        pass
+        model = Sequential()
+
+        model.add(Conv2D(8, (3, 3), activation='relu', input_shape=(150, 150, 3)))
+        model.add(MaxPooling2D((2, 2)))
+        model.summary()
+
+        model.add(Conv2D(16, (3, 3), activation='relu'))
+        model.add(MaxPooling2D((2, 2)))
+        model.summary()
+
+        model.add(Conv2D(16, (3, 3), activation='relu'))
+        model.add(MaxPooling2D((2, 2)))
+        model.summary()
+
+        model.add(Flatten())
+        model.add(Dense(16, activation='relu'))
+        model.add(Dense(16, activation='relu'))
+        model.summary()
+        
+        # 3 classes with softmax
+        model.add(Dense(3, activation='softmax'))
+
+        self.model = model
     
     def _compile_model(self):
         # Your code goes here
         # you have to compile the keras model, similar to the example in the writeup
-        pass
+        # example:
+        self.model.compile(
+            optimizer=RMSprop(learning_rate=0.001),
+            loss='categorical_crossentropy',
+            metrics=['accuracy'],
+        )
--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # P6Repo
+
+## Creating Virtual Environment
+Make sure you have [`pip`](https://packaging.python.org/en/latest/tutorials/installing-packages/) installed (should already have).
+
+Run the command in the repo directory to create the virtual environment `venv`.
+```
+virtualenv venv -p $(which python3.8)
+```
+This is pretty much required to use the deprecated packages used for the assignment.
+Newer versions of Python / package managers will not find or install the packages
+in the `requirements.txt` file.  
+
+Then, run the following to start the virtual environment.
+```
+source venv/bin/activate
+```
+Next, run the following to install all of the needed packages for the assignment:
+```
+python3 -m pip install -r requirements.txt
+```
+Make sure you have sufficient storage for these packages and the images.  
+
+Exit the virtual enviroment using the `deactivate` command.
+
+## Initial Network
+![Graph](P6/assets/graph.png "Initial Network")
